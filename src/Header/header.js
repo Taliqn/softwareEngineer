5712d4c6af830675b204ad3b61efe637ff225b9d
Denis <nesterov78@mail.ru>
2021-05-03 08:26:24 +0300

    Build Versel 1

--- src/Header/header.js
+++ src/Header/header.js
@@ -6,8 +6,32 @@ import {NavLink} from "react-router-dom";
 const Header = () => {
     return(
         <div className="pageHeader">
-            <bottom className='bottomH' ><NavLink to='/' className='aHeader'> <small>IT Consulting</small></NavLink>
-                </bottom>
+
+                <nav className='menu'>
+                    <ul className='menu__list'>
+                        <li>
+                            <NavLink className = 'menu__link' to='/' > <small>Consulting</small></NavLink>
+                            <ul className='sub-menu__list'>
+                                <li>
+                                    <NavLink className = 'sub-menu__link' to='/' > <small>Consulting 2</small></NavLink>
+                                </li>
+                                <li>
+                                    <NavLink className = 'sub-menu__link' to='/' > <small>Consulting 2</small></NavLink>
+                                </li>
+                                <li>
+                                    <NavLink className = 'sub-menu__link' to='/' > <small>Consulting 2</small></NavLink>
+                                </li>
+                                <li>
+                                    <NavLink className = 'sub-menu__link' to='/' > <small>Consulting 2</small></NavLink>
+                                </li>
+                                <li>
+                                    <NavLink className = 'sub-menu__link' to='/' > <small>Consulting 2</small></NavLink>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+
             </div>
     )
 }
